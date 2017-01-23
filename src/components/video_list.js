b9f4addf75237b3772bfa594aae005aca30efe4c
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-23 15:41:08 +0800

    add initial component file
    - add youtube api key

---
+++ src/components/video_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class VideoList extends React.Component {
+	render () {
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAubILUFEzObss2Xm4b_BztnBsai77Gc0E';
+
 const App = () => {
 	return <div>Hi!</div>;
 }
