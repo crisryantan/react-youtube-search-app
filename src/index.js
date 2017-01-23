f255c4944ddf4cfa961e439f05053ab9b42faef6
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-23 14:22:46 +0800

    add initial files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+ReactDOM.render( <App />, document.querySelector( '.container' ) );
